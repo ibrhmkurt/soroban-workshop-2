af6d2fa5702bf8d30fc6bd93fab875ba10c30a39
kökbirasi <kokbirasi@kokbirasi-MacBook-Air.local>
2024-10-08 17:58:59 +0300

    i

--- contracts/hello_world/src/lib.rs
+++ contracts/hello_world/src/lib.rs
@@ -1,14 +1,15 @@
-#![no_std]
-use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
-
-#[contract]
-pub struct HelloContract;
-
-#[contractimpl]
-impl HelloContract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
-    }
+pub fn add(left: u64, right: u64) -> u64 {
+ left + right   
 }
 
-mod test;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result: u64 = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
